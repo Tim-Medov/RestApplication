321e90389915b4d52a4c719295ca7adac1012c2a
Timur Amanmamedov <amanmamedow@gmail.com>
2022-07-15 17:52:10 -0400

    create

---
+++ src/main/java/enterprise/services/PersonService.java
@@ -0,0 +1,36 @@
+
+package enterprise.services;
+
+import enterprise.models.Person;
+import enterprise.repositories.PersonRepository;
+import enterprise.utils.PersonNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PersonService {
+
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    public List<Person> findAll() {
+        return personRepository.findAll();
+    }
+
+    public Person findOne(int id) {
+        Optional<Person> optionalPerson = personRepository.findById(id);
+        return  optionalPerson.orElseThrow(PersonNotFoundException::new);
+    }
+
+    @Transactional
+    public void save(Person person) {
+        personRepository.save(person);
+    }
+}
